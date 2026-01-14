10fc5d29894bc9c35f206150aa4ad06ad48112ae
llanerosolitariio-commits <llanerosolitariio@gmail.com>
2026-01-14 19:30:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
-app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => { io.emit('chat message', msg); });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+  socket.on('chat message', (data) => {
+    // Ahora 'data' contiene {user: "Nombre", text: "Hola"}
+    // Reenviamos ese mismo objeto a todos
+    io.emit('chat message', data);
+  });
+});
+
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => { console.log('Servidor en puerto ' + PORT); });
+server.listen(PORT, () => {
+  console.log('Servidor en puerto ' + PORT);
+});
